ff1844c9f2781588274553f38596f31162b4279a
Matt <m.d.schulze425@gmail.com>
2020-09-13 19:38:17 -0600

    Added toggle button to spell component to allow the spell details to be toggled on/off(visible/hidden)

--- src/Components/Spell/Spell.js
+++ src/Components/Spell/Spell.js
@@ -1,23 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./Spell.css";
 
-const print = x => {
-    console.log(x);
-}
-
 const Spell = (props) => {
+    const [visible, setVisible] = useState(true);
 
     return(
 
         <div className='SpellsByLevel-Spell' id={props.spell.spellName}>
         <span><h3>{props.spell.spellName} <img className='SpellsByLevel-Image' src={props.spell.image} width="100px" alt={props.spell.spellName} /></h3></span>
-        <ul>
-            <li>Spell School: {props.spell.spellSchool}</li>
-            <li>Casting Time: {props.spell.castingTime}</li>
-            <li>Range: {props.spell.range}</li>
-            <li>Attack Type or Save: {props.spell.attackOrSave}</li>
-        </ul>
-        <button onClick={() => {print(props)}}>Hide Spell</button>                    
+        {visible === true ? 
+            <ul>
+                <li>Spell School: {props.spell.spellSchool}</li>
+                <li>Casting Time: {props.spell.castingTime}</li>
+                <li>Range: {props.spell.range}</li>
+                <li>Attack Type or Save: {props.spell.attackOrSave}</li>
+            </ul> : null}
+        <button onClick={() => setVisible(!visible)}>Hide Details</button>                    
         </div>
             
     )
