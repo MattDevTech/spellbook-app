dc1fe7bafe17bf9f4c39aff95c25ea24d7084c43
Matt <m.d.schulze425@gmail.com>
2020-09-27 12:06:36 -0600

    began writing tests for components

---
+++ src/Components/Spell/Spell.test.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import Spell from './Spell';
+
+// test('it renders without crashing', () => {
+//   render(<Spell spellName= "Acid Splash" spellSchool= "Conjuration" />);
+// })
---
+++ src/Components/SpellsByLevel/SpellsByLevel.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// import { render } from '@testing-library/react';
+// import SpellsByLevel from './SpellsByLevel';
+
+// test('it renders without crashing', () => {
+//   render(<SpellsByLevel />)
+// })
